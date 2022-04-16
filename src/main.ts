1d7b3969e7f81f5b5a1e4c58f0aa671d4037606e
Jacob Martin <jacob@trendcapital.com>
2022-04-16 16:13:50 -0700

    working flood

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { ConfigService } from './services/config.service';
 import { default as pino } from 'pino';
+
+import { ConfigService } from './services/config.service';
+import { TwitterService } from './services/twitter.service';
 
 (async () => {
   const configService = new ConfigService();
   const logger = pino({ level: configService.logLevel });
+  const twitterService = new TwitterService(configService, logger);
 
   logger.info('Starting up');
-})();
+  twitterService.flood();
+})().catch((e) => {
+  throw e;
+});
--- src/services/twitter.service.ts
+++ src/services/twitter.service.ts
@@ -1,10 +1,33 @@
-import { ConfigService } from './config.service';
 import * as needle from 'needle';
+import { Logger } from 'pino';
+
+import { ConfigService } from './config.service';
 
 export class TwitterService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(private readonly configService: ConfigService, private readonly logger: Logger) {}
 
-  public stream() {
-    needle.get();
+  public flood() {
+    this.logger.info(this.flood.name);
+
+    const stream = needle.get(
+      'https://api.twitter.com/2/tweets/sample/stream?tweet.fields=text,attachments&media.fields=url',
+      {
+        headers: {
+          'User-Agent': 'v2SampleStreamJS',
+          Authorization: `Bearer ${this.configService.twitterBearerToken}`,
+        },
+
+        timeout: 20 * 1000,
+      },
+    );
+
+    stream.on('data', (data) => {
+      try {
+        const tweet = JSON.parse(data);
+        this.logger.info({ tweet }, 'Tweet');
+      } catch (e) {
+        this.logger.error({ data }, e.message);
+      }
+    });
   }
 }
