bfe5fb5e4ff4d3db45e3eb543ce9a2bc173ea302
Jacob Martin <jacob@trendcapital.com>
2022-04-16 17:32:26 -0700

    set up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,17 @@
+import { useEffect, useState } from 'react';
+
 export default function App() {
-  return <div className="App">hello world</div>;
+  const [image, setImage] = useState('Image Placeholder :)');
+
+  useEffect(() => {
+    const eventSource = new EventSource(`http://localhost:3000/image`);
+    eventSource.onmessage = (e) => setImage(e.data);
+    return () => {
+      eventSource.close();
+    };
+  }, [])
+
+  return <div className="App">
+    <p>{image}</p>
+  </div>;
 }
